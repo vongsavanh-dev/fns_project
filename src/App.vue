21e42e084db8db39c3cb2ff19a3b329824457e7e
vongsavanh <vongsavanhdev@gmail.com>
2023-03-28 14:36:27 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <UserIndex/>
+  <RouterView/>
+</template>
+<script setup>
+import UserIndex from "@/views/User/Index.vue"
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/User/Index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="user-container">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Index"
+}
+</script>
+
+<style scoped lang="scss">
+.user-container {
+  width: 100%;
+  height: 100vh;
+  background: red;
+}
+</style>
